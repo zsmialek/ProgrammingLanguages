8904e9db96c7d44b3dac10da4c3d263acb54956b
Zach Smialek <zsmialek@mines.edu>
2016-09-12 16:47:18 -0600

    Selekton of rregular expressions.

---
+++ RegExp/RegExpFun.rb
@@ -0,0 +1,27 @@
+module RegExpFun
+
+  def get_language(str):
+    language = ""
+    return language
+  end
+
+
+  def get_adjective(str):
+    adjective = ""
+ 
+    return adjective
+  end
+
+  def get_price(str):
+    price = ""
+    return price 
+  end
+
+  
+  def is_complex(str):
+    complex_num = ""
+    return complex_num
+  end
+
+
+end
