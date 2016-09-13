7c89136ce5b3fe3c826424bf4f0dd39a41993d51
Zach Smialek <zsmialek@mines.edu>
2016-09-12 18:11:09 -0600

    Got rid of :

--- RegExp/RegExpFun.rb
+++ RegExp/RegExpFun.rb
@@ -1,24 +1,24 @@
 module RegExpFun
 
-  def get_language(str):
+  def get_language(str)
     language = ""
     return language
   end
 
 
-  def get_adjective(str):
+  def get_adjective(str)
     adjective = ""
- 
     return adjective
   end
 
-  def get_price(str):
+
+  def get_price(str)
     price = ""
     return price 
   end
 
   
-  def is_complex(str):
+  def is_complex(str)
     complex_num = ""
     return complex_num
   end
