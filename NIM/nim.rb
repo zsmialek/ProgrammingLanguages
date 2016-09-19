1db3d3df8c0cf3e85d468ef7aebe751de96e2924
Zach Smialek <zsmialek@mines.edu>
2016-09-19 10:54:09 -0600

    Added NIM

---
+++ NIM/nim.rb
@@ -0,0 +1,36 @@
+class Game
+
+  def initialize()
+    @board_one = [1,3,5,7]
+    @board_two = [4,3,7]
+    @board_selection = 0
+    welcome_screen()
+  end
+
+
+  def welcome_screen()
+    welcome_message = "Welcome to NIM"
+    board_config_message = "Select board configuration (1-2): "
+
+    puts(welcome_message)
+    puts("1: #{@board_one} \n2: #{@board_two} \n#{board_config_message}")
+    @board_selection = $stdin.gets.chomp()
+
+  end
+
+  
+  def draw_board(user_selection)
+    
+  end
+end
+
+
+class Player
+  def initalize()
+
+  end
+
+end
+
+game = Game.new()
+
