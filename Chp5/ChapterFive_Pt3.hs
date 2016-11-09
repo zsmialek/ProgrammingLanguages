a1e94c981b57e26ed3ace0a7df5ee72b15f2fbeb
Zach Smialek <zsmialek@mines.edu>
2016-11-08 18:25:24 -0700

    Fixed question

--- Chp5/ChapterFive_Pt3.hs
+++ Chp5/ChapterFive_Pt3.hs
@@ -1,3 +1,8 @@
+-- Zach Smialek
+-- Chapter 5 Pt 3
+--
+module ChapterFive_Pt3 where
+
 getUserName :: String -> String
 getUserName [] = []
 getUserName (x:xs) 
@@ -19,3 +24,27 @@ discountedItems p x = foldr (\x acc -> (x - x*p):acc) [] x
 
 anyBigNumbers :: Int -> [Int] -> Bool
 anyBigNumbers thres x = foldr (\x acc -> if x > thres then True else acc) False x
+
+multTableRow :: Int -> [Int]
+multTableRow x = map ($x) [(1*),(2*),(3*),(4*),(5*),(6*),(7*),(8*),(9*),(10*)]
+
+f1 :: (Num a, Fractional a) => a-> a
+f1 x = 2*x + 3
+
+f2 :: (Num a, Fractional a) =>a -> a
+f2 x = x*x
+
+f3 :: (Num a, Fractional a) => a -> a
+f3 x = 0.5*x - 1.5
+
+--testInverses f1 f2  
+--  | (f1 . f2) 1 == (f2 . f1) 1 = True
+--  | otherwise = False
+
+--testInverses :: f->g -> Bool
+testInverses f1 f2 
+  | map (f1 .f2) [1,34,56,7,-7,3,3,-2,-1] == map (f2 . f1 ) [1,34,56,7,-7,3,3,-2,-1] = True
+  | otherwise = False
+-- and_1 (repeat False) returns False while and_2 ( repeat False ) function gets stuck in a loop.
+-- Since repeat is an infinte list, foldl cannot reach an empty list to break out of the function
+-- call.  
